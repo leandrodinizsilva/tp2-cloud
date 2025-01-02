584f05bc39ece2e68aa52404b7d29144f9cc7d01
Leandro Diniz <leandrodinizsilva9@gmail.com>
2025-01-02 00:27:08 -0300

    debug

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 
+console.log(process.env);
 const API_URL = process.env.API_URL || "http://localhost:31331";
 
 function App() {
